e9b740c59f76c4a992759c2bbb0bfd114204863b
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2020-12-13 10:05:32 -0500

    Added build and run scripts and gitignore

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp src:lib/commons-logging.jar:lib/log4j-1.2.14.jar:lib/pdfbox-1.8.2.jar:lib/pdfxtk-backend.jar:lib/xercesImpl.jar  at.ac.tuwien.dbai.pdfwrap.MeasureRecognitionPerformance $*
